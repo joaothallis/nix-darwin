0aef1615b44e5fecb8044e8a488099fd51940132
João Thallis <joaothallis@icloud.com>
2024-08-28 19:21:55 -0300

    Use local hostname

--- flake.nix
+++ flake.nix
@@ -40,12 +40,12 @@
   in
   {
     # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#simple
-    darwinConfigurations."simple" = nix-darwin.lib.darwinSystem {
+    # $ darwin-rebuild build --flake .#Joaos-MacBook-Pro
+    darwinConfigurations."Joaos-MacBook-Pro" = nix-darwin.lib.darwinSystem {
       modules = [ configuration ];
     };
 
     # Expose the package set, including overlays, for convenience.
-    darwinPackages = self.darwinConfigurations."simple".pkgs;
+    darwinPackages = self.darwinConfigurations."Joaos-MacBook-Pro".pkgs;
   };
 }
