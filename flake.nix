aa82dab04c075ec1d4947fbc8b9f5d7718e2c5fa
João Thallis <joaothallis@icloud.com>
2024-11-24 17:53:44 -0300

    Remove comment

--- flake.nix
+++ flake.nix
@@ -111,7 +111,6 @@
 
           # Create /etc/zshrc that loads the nix-darwin environment.
           programs.zsh.enable = true; # default shell on catalina
-          # programs.fish.enable = true;
 
           programs.tmux.enable = true;
           programs.tmux.enableVim = true;
