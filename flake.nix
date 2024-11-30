5c6014623089d31310c6d7b679caa3e3948810e0
João Thallis <joaothallis@icloud.com>
2024-11-30 15:48:03 -0300

    Set tmux terminal-features to rio

--- flake.nix
+++ flake.nix
@@ -119,7 +119,7 @@
             enableVim = true;
             extraConfig = ''
                           	set-option -g focus-events on
-              		set-option -a terminal-features 'xterm-256color:RGB'
+              		set-option -a terminal-features 'rio:RGB'
 
               		set -g default-command fish
             '';
