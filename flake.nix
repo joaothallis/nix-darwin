3728796ddb92a56032bcbaa26fc23a3091638f1e
João Thallis <joaothallis@icloud.com>
2024-12-12 22:19:21 -0300

    Set tmux history-limit to 4000

--- flake.nix
+++ flake.nix
@@ -124,6 +124,7 @@
               		set-option -a terminal-features 'rio:RGB'
 
               		set -g default-command fish
+              		set -g history-limit 4000
             '';
           };
 
