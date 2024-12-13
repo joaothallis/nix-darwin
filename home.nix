83b214a51f23f2d287cddbf04bb762e65c5c9339
João Thallis <joaothallis@icloud.com>
2024-12-12 22:19:52 -0300

    Add `HEAD` to `git --no-pager diff` command
    It's to get all changes that are not committed

--- home.nix
+++ home.nix
@@ -52,7 +52,7 @@
       gr = "git reset --hard";
       gcd = "git checkout develop";
       gc- = "git checkout -";
-      gcai = "git --no-pager diff | mods --model gemini-1.5-flash-latest 'write a commit message for this patch. also write the long commit message. use semantic commits. break the lines at 80 chars.' >.git/gcai; git commit -a -F .git/gcai -e";
+      gcai = "git --no-pager diff HEAD | mods --model gemini-1.5-flash-latest 'write a commit message for this patch. also write the long commit message. use semantic commits. break the lines at 80 chars.' >.git/gcai; git commit -a -F .git/gcai -e";
       gpr = "gh pr create --assignee @me";
       r = "request_review";
       dps = "docker compose ps";
