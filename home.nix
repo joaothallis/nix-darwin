6db83ec32072dd1866eb83d3f7f01ca464ac81eb
João Thallis <joaothallis@icloud.com>
2025-01-22 23:20:41 -0300

    feat(home.nix): Add run_mix_test function
    This commit adds a new function called `run_mix_test` to the
    home.nix file. This function aims to improve the developer
    workflow by automatically determining which tests to run based on
    the files changed in the current git working tree.
    
    The `run_mix_test` function uses `git status` to identify
    changed files. It then transforms the paths of changed files
    under `lib/` to their corresponding test file paths under
    `test/`. Finally, it constructs and executes a `mix test`
    command with the appropriate test file arguments.
    
    This significantly improves the efficiency of running tests by
    only running the tests related to the files that been
    modified, minimizing test execution time.

--- home.nix
+++ home.nix
@@ -102,6 +102,31 @@
       open_coverage_report = ''
         open cover/Elixir.$argv.html
       '';
+      run_mix_test = ''
+        # Get the list of file paths from `git status`
+        set file_paths (git status --porcelain=v1 --untracked-files=all | awk '{print $2}' | grep .ex)
+
+        # Initialize an array to store the transformed paths
+        set transformed_paths
+
+        # Process the file paths
+        for path in $file_paths
+            if string match -q "lib/*" $path
+                # Replace 'lib/' with 'test/' and change '.ex' to '_test.exs'
+                set transformed_path (echo $path | sed -E 's|^lib/|test/|; s|\.ex$|_test.exs|')
+                set transformed_paths $transformed_paths $transformed_path
+            else
+                # Keep the path as is
+                set transformed_paths $transformed_paths $path
+            end
+        end
+
+        # Output the mix test command
+        echo "mix test $transformed_paths"
+
+        # Run the mix test command
+        eval "mix test $transformed_paths"
+            	'';
     };
   };
 
