ead762636976cd7ca121f99bebabc70acfc2e7e6
João Thallis <joaothallis@icloud.com>
2025-03-28 20:19:11 -0300

    Add vim.lsp.references keymap

--- home.nix
+++ home.nix
@@ -65,10 +65,11 @@
             }
             require'lspconfig'.ts_ls.setup{}
 
-                        -- Basic LSP keybindings
-                        vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
-                        vim.keymap.set('n', 'K', vim.lsp.buf.hover)
-                        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename)
+            -- Basic LSP keybindings
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
+            vim.keymap.set('n', 'gr', vim.lsp.buf.references)
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover)
+            vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename)
           '';
         }
         {
