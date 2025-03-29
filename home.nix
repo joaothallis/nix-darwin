e31d77fd44521a72bc8729b664fa6027aa9023e2
João Thallis <joaothallis@icloud.com>
2025-03-29 00:03:03 -0300

    feat(nvim): use nvim-cmp

--- home.nix
+++ home.nix
@@ -45,6 +45,79 @@
         }
       '';
       plugins = with pkgs.vimPlugins; [
+        cmp-nvim-lsp
+        cmp-buffer
+        cmp-path
+        cmp-cmdline
+
+        {
+          plugin = nvim-cmp;
+          type = "lua";
+          config = ''
+            		        -- Set up nvim-cmp.
+              local cmp = require'cmp'
+
+              cmp.setup({
+                snippet = {
+                  -- REQUIRED - you must specify a snippet engine
+                  expand = function(args)
+                    vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+                    -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                    -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+                    -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+                    -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+
+                    -- For `mini.snippets` users:
+                    -- local insert = MiniSnippets.config.expand.insert or MiniSnippets.default_insert
+                    -- insert({ body = args.body }) -- Insert at cursor
+                    -- cmp.resubscribe({ "TextChangedI", "TextChangedP" })
+                    -- require("cmp.config").set_onetime({ sources = {} })
+                  end,
+                },
+                window = {
+                  -- completion = cmp.config.window.bordered(),
+                  -- documentation = cmp.config.window.bordered(),
+                },
+                mapping = cmp.mapping.preset.insert({
+                  ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                  ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                  ['<C-Space>'] = cmp.mapping.complete(),
+                  ['<C-e>'] = cmp.mapping.abort(),
+                  ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                }),
+                sources = cmp.config.sources({
+                  { name = 'nvim_lsp' },
+                  { name = 'vsnip' },
+                }, {
+                  { name = 'buffer' },
+                })
+              })
+
+              -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+              cmp.setup.cmdline({ '/', '?' }, {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = {
+                  { name = 'buffer' }
+                }
+              })
+
+              -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+              cmp.setup.cmdline(':', {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = cmp.config.sources({
+                  { name = 'path' }
+                }, {
+                  { name = 'cmdline' }
+                }),
+                matching = { disallow_symbol_nonprefix_matching = false }
+              })
+
+                        	      '';
+        }
+
+        cmp-vsnip
+        vim-vsnip
+
         {
           plugin = nvim-treesitter.withAllGrammars;
           type = "lua";
@@ -59,34 +132,37 @@
           plugin = elixir-tools-nvim;
           type = "lua";
           config = ''
-	    elixirls = require("elixir.elixirls")
+                        	    elixirls = require("elixir.elixirls")
 
-            require("elixir").setup({
-              nextls = {enable = true},
-              elixirls = {enable = true,
-		 settings = elixirls.settings {
-		      autoBuild = true,
-		      dialyzerEnabled = true,
-		      incrementalDialyzer = true,
-		      fetchDeps = true,
-		      enableTestLenses = false
-		    }
-	      },
-              projectionist = {enable = false},
-            })
+                                    require("elixir").setup({
+                                      nextls = {enable = true, init_options = {experimental = {enable = true}}},
+                                      elixirls = {enable = true,
+                        		 settings = elixirls.settings {
+                        		      autoBuild = true,
+                        		      dialyzerEnabled = true,
+                        		      incrementalDialyzer = true,
+                        		      fetchDeps = true,
+                        		      enableTestLenses = false
+                        		    }
+                        	      },
+                                      projectionist = {enable = false},
+            			    capabilities = capabilities
+
+                                    })
           '';
         }
         {
           plugin = nvim-lspconfig;
           type = "lua";
           config = ''
-            require'lspconfig'.ts_ls.setup{}
+            	    local capabilities = require('cmp_nvim_lsp').default_capabilities()
+                        require'lspconfig'.ts_ls.setup{capabilities=capabilities}
 
-            -- Basic LSP keybindings
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
-            vim.keymap.set('n', 'gr', vim.lsp.buf.references)
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover)
-            vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename)
+                        -- Basic LSP keybindings
+                        vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
+                        vim.keymap.set('n', 'gr', vim.lsp.buf.references)
+                        vim.keymap.set('n', 'K', vim.lsp.buf.hover)
+                        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename)
           '';
         }
         {
